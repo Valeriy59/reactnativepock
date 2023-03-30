02d2e51dd6d7c10d359e1450a8881968720a9ba8
Valeriy Litvinov <valitvinoff@mail.ru>
2023-03-30 16:01:54 +0300

    first commit

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,28 +1,55 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {api, PokemonItem} from "../../api/api";
+import {FlatList, Image, ListRenderItem, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {api, Pokemon, PokemonItem} from "../../api/api";
 import {NUM_COLUMNS, PADDING, WIDTH} from "../../constant/constant";
 import {useAppNavigation} from "../../types";
 
+type Item = {
+  name: string
+  icon: string
+  id: number
+  key: number
+  url: string
+}
 
 export const HomeScreen = () => {
   const {navigate} = useAppNavigation()
 
-  const [allPokemon, setAllPokemon] = useState<PokemonItem[]>([])
+  const [allPokemon, setAllPokemon] = useState<Item[]>([])
+  const [pokemonImg, setPokemonImg] = useState<Pokemon[]>([])
+
   useEffect(() => {
-    api.getAllPokemon().then((resp) => {
-      setAllPokemon(resp.data.results)
+    api.getAllPokemon()
+      .then((resp) => {
+      let results = resp.data.results
+      results.forEach((pokemon, index) => {
+        api.getPokemonById(pokemon.url).then((resp) => {
+          setAllPokemon((prevState) => [...prevState, {
+            id: resp.data.id,
+            key: index + 1,
+            icon: resp.data.sprites.other['official-artwork'].front_default,
+            name: resp.data.name,
+            url: pokemon.url
+          } ])
+          resp.data
+        })
+      })
     })
   }, [])
 
-  const renderItem: ListRenderItem<PokemonItem> = ({item}) => {
+  const renderItem: ListRenderItem<Item> = ({item, index}) => {
     return (
       <TouchableOpacity
         style={styles.item}
-        onPress={() => {navigate('Details', {url: item.url})}}
+        onPress={() => {
+          navigate('Details', {url: item.url})
+        }}
       >
         <View>
           <Text style={styles.itemText}>{item.name}</Text>
+          {pokemonImg[0] && <Image
+                              style={{width: 50, height: 50}}
+                              source={{uri: item.icon}}/>}
         </View>
       </TouchableOpacity>
     )
